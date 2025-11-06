491d2fa1ec7c663d2af013b91b5120b36415beac
Joshua Cajina <josh.cajina@gmail.com>
2025-11-05 23:27:59 -0600

    refactor(backend): Modified Loan related entities

---
+++ apps/backend/Fundo/Fundo.Loan.Domain/Common/AuditableEntity.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fundo.Loan.Domain.Common;
+public abstract class AuditableEntity : BaseEntity
+{
+    public DateTime CreatedAtUtc { get; set; }
+    public DateTime? UpdatedAtUtc { get; set; }
+    public DateTime? LastCompletedAtUtc { get; set; }
+}
---
+++ apps/backend/Fundo/Fundo.Loan.Domain/Entities/LoanRequest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Fundo.Loan.Domain.Common;
+
+namespace Fundo.Loan.Domain.Entities;
+
+public sealed class LoanRequest : AuditableEntity
+{
+    public decimal Amount { get; set; }
+    public string Purpose { get; set; } = string.Empty;
+    public LoanStatus Status { get; set; }
+    public DateTime RequestedDate { get; set; }
+    public DateTime? DecisionDate { get; set; }
+
+    public string BorrowerId { get; set; }
+    public Borrower Borrower { get; set; }
+
+    public string? LoanOfficerId { get; set; }
+    public LoanOfficer? LoanOfficer { get; set; }
+}
